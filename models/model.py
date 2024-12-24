6946f2f0c1ed0869a728986d1d2d16b2894a8ae8
Bassant Medhat <85830264+bassantmedhat@users.noreply.github.com>
2024-12-23 18:08:13 -0800

    Add files via upload
    Fixing a minor issue

--- models/model.py
+++ models/model.py
@@ -1,6 +1,8 @@
 from torchvision.models.segmentation import deeplabv3_resnet50
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
 
 
 class DeepLabFeatureExtractor(nn.Module):
@@ -8,24 +10,26 @@ class DeepLabFeatureExtractor(nn.Module):
         super(DeepLabFeatureExtractor, self).__init__()
         self.backbone = model.backbone  # Encoder
         self.classifier = model.classifier  # Decoder
+        # print(model)
 
     def forward(self, x):
         encoder_features = []
-
-        # Pass through the encoder layers
+        decoder_features = []
+        
+        # Extract features from backbone (encoder)
         x = self.backbone.conv1(x)
         x = self.backbone.bn1(x)
         x = self.backbone.relu(x)
         x = self.backbone.maxpool(x)
-
-        # Extract features at specific layers
+        
         for name, layer in self.backbone.named_children():
             if "layer" in name:
                 x = layer(x)
-                if "layer2" in name:  # Add the output from layer2 to encoder_features
+                if "layer2" in name:
                     encoder_features.append(x)
-
-        # Perform the decoder forward pass
+        
+        # Decoder forward pass
         decoder_output = self.classifier(x)
-
-        return decoder_output, encoder_features
+        decoder_features.append(decoder_output)
+        
+        return decoder_features, encoder_features
